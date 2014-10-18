0de1dd1ab52b4d62fc2ed1f1083e576b0cbd32b5
Rafael Fiuza <guiloyins@gmail.com>
2014-10-18 19:20:20 -0300

    added dynamic url

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 class PagesController < ApplicationController
-  CALLBACK_URL = "#{ root_url }/pages/pocket_callback"
   def step1
-    binding.pry
   end
 
   def step2
@@ -11,13 +9,13 @@ class PagesController < ApplicationController
   end
 
   def pocket_login
-    session[:code] = Pocket.get_code(:redirect_uri => CALLBACK_URL)
-    new_url = Pocket.authorize_url(:code => session[:code], :redirect_uri => CALLBACK_URL)
+    session[:code] = Pocket.get_code(:redirect_uri => pocket_callback_url)
+    new_url = Pocket.authorize_url(:code => session[:code], :redirect_uri => pocket_callback_url)
     redirect_to new_url
   end
 
   def pocket_callback
-    result = Pocket.get_result(session[:code], :redirect_uri => CALLBACK_URL)
+    result = Pocket.get_result(session[:code], :redirect_uri => pocket_callback_url)
     session[:access_token] = result['access_token']
     redirect_to root_path
   end
