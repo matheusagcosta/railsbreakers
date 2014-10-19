c45212626c84b410b290de530face5bb54ad0b90
Rafael Fiuza <guiloyins@gmail.com>
2014-10-19 02:51:31 -0200

    added spec to add_link method

--- spec/providers/pocket_provider_spec.rb
+++ spec/providers/pocket_provider_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe PocketProvider do
+  let!(:provider) { PocketProvider.new(['foo.com', 'lelek.com.br']) }
 
   describe "initialize" do
     it "does set provider links" do
-      provider = PocketProvider.new(['foo.com', 'lelek.com.br'])
       expect(provider.links).to eq(['foo.com', 'lelek.com.br'])
+    end
+  end
+
+  describe "#add_link" do
+    it "does add links to added_links" do
+      provider.add_link('railsrumble.com')
+      expect(provider.added_links).to eq(['railsrumble.com'])
     end
   end
 end
